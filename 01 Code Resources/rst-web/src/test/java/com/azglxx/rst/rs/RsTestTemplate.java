1d56d67d634f1fc51e59ff1d0bd8ea98f34751c6
Arian Zhang <arian_zhang@foxmail.com>
2013-07-20 09:09:46 -0700

    测试通过

--- 01 Code Resources/rst-web/src/test/java/com/azglxx/rst/rs/RsTestTemplate.java
+++ 01 Code Resources/rst-web/src/test/java/com/azglxx/rst/rs/RsTestTemplate.java
@@ -10,51 +10,53 @@ import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.NewCookie;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.azglxx.rst.rs.handler.GsonMessageBodyHandler;
+import com.azglxx.common.utils.JsonUtils;
+import com.azglxx.rst.rs.constants.PathConst;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
 
-public class RsTestTemplate {
+public abstract class RsTestTemplate {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RsTestTemplate.class);
 
-	protected String URL = "http://127.0.0.1:8080/rstweb";
-	protected String ROOT = "/rest/root";
-	protected ClientConfig clientConfig;
-	protected Client client;
-	protected WebResource resource;
-	protected Cookie cookies;
+	protected static String URL = "http://localhost:8080/rst-web/rest";
+	protected static ClientConfig clientConfig;
+	protected static Client client;
+	protected static WebResource resource;
+	protected static Cookie cookie;
 
-	@Before
-	public void beforeTest() throws IOException {
+	@BeforeClass
+	public static void beforeTestClass() throws IOException {
 		clientConfig = new DefaultClientConfig();
-		clientConfig.getClasses().add(GsonMessageBodyHandler.class);
-		client = Client.create(this.clientConfig);
+		client = Client.create(clientConfig);
 		client.setFollowRedirects(true);
 		resource = client.resource(URL);
 		for (NewCookie cookie : login()) {
-			cookies = cookie;
+			RsTestTemplate.cookie = cookie;
 		}
 	}
 
-	protected List<NewCookie> login() {
+	protected static List<NewCookie> login() {
 		HashMap<String, String> userInfo = new HashMap<String, String>();
 		userInfo.put("username", "SuperAdmin");
 		userInfo.put("password", "888888");
-		ClientResponse response = resource.path(ROOT + "/user/login").accept(MediaType.APPLICATION_JSON)
-				.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, userInfo);
+		ClientResponse response = resource.path(PathConst.RIGHTS_MGT + "/user/login")
+				.accept(MediaType.APPLICATION_JSON).type(MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class, JsonUtils.toJson(userInfo));
 		assertOk(response);
 		return response.getCookies();
 	}
 
-	protected void assertOk(ClientResponse cr) {
+	protected abstract WebResource pathResource();
+
+	protected static void assertOk(ClientResponse cr) {
 		LOGGER.info("reponse status is " + cr.getStatus());
 		assertEquals("reponse status is not OK", 200, cr.getStatus());
 	}
