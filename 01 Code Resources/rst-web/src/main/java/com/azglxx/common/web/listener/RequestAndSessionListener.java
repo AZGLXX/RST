cf80d62a7108497358a199c4acb4432bf244b67b
ArianZhang <arian_zhang@foxmail.com>
2013-07-20 20:29:58 +0800

    add filter and listener

---
+++ 01 Code Resources/rst-web/src/main/java/com/azglxx/common/web/listener/RequestAndSessionListener.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.azglxx.common.web.listener;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextListener;
+
+/**
+ * 
+ * @author Arian Zhang
+ * @email arian_zhang@foxmail.com
+ * @date 2013-7-20
+ * @version 1.0
+ */
+@WebListener
+public class RequestAndSessionListener extends RequestContextListener implements HttpSessionListener {
+	private final static Logger LOGGER = LoggerFactory.getLogger(RequestAndSessionListener.class);
+
+	private AtomicInteger count = new AtomicInteger(0);
+
+	@Override
+	public void sessionCreated(HttpSessionEvent sessionEvent) {
+		count.incrementAndGet();
+		if (LOGGER.isDebugEnabled()) {
+			HttpSession session = sessionEvent.getSession();
+			LOGGER.debug("\n===========session created, id=" + session.getId() + ",max inactive interval is"
+					+ session.getMaxInactiveInterval() + ",current session count : " + count + ".==========\n");
+		}
+	}
+
+	@Override
+	public void sessionDestroyed(HttpSessionEvent sessionEvent) {
+		count.decrementAndGet();
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("\n================session destroyed. current session count:" + count
+					+ "====================\n");
+		}
+	}
+
+}
