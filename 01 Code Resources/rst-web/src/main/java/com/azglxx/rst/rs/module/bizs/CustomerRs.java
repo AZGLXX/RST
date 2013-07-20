1c20267fd6d0f60bf519afe70cb7cb481c46bffe
Arian Zhang <arian_zhang@foxmail.com>
2013-07-20 07:54:50 -0700

    add customer management

---
+++ 01 Code Resources/rst-web/src/main/java/com/azglxx/rst/rs/module/bizs/CustomerRs.java
@@ -0,0 +1,20 @@
+package com.azglxx.rst.rs.module.bizs;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Service;
+
+import com.azglxx.rst.rs.constants.PathConst;
+
+/**
+ * customer information management
+ * @author Arian Zhang
+ * @email arian_zhang@foxmail.com
+ * @date 2013-7-20
+ * @version 1.0
+ */
+@Path(PathConst.BIZS + "/customer")
+@Service
+public class CustomerRs {
+
+}
