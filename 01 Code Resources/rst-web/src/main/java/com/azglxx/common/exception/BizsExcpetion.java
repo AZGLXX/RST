0a46de0ee070c7208e0da879fb3b25db0d5f9220
ArianZhang <arian_zhang@foxmail.com>
2013-07-22 11:49:17 +0800

    add exception

---
+++ 01 Code Resources/rst-web/src/main/java/com/azglxx/common/exception/BizsExcpetion.java
@@ -0,0 +1,30 @@
+package com.azglxx.common.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Description:
+ * 
+ * @author Arian Zhang
+ * @email hzhang@digitnexus.com
+ * @Date 2013-7-22 上午11:41:11
+ * @since v1.0.0
+ */
+public class BizsExcpetion extends RuntimeException {
+
+    private final static Logger logger = LoggerFactory.getLogger(BizsExcpetion.class);
+
+    public BizsExcpetion(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public BizsExcpetion(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BizsExcpetion(Throwable cause) {
+        super(cause);
+    }
+
+}
